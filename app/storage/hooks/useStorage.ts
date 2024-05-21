3b1a998161ba70a7dec81800413767683eba14ab
morieeeenyo <uint2435@gmail.com>
2024-05-21 17:33:26 +0900

    chore: formatの実行

--- app/storage/hooks/useStorage.ts
+++ app/storage/hooks/useStorage.ts
@@ -1,31 +1,35 @@
-import s3 from '@aws-sdk/client-s3';
-import axios from 'axios';
+import s3 from '@aws-sdk/client-s3'
+import axios from 'axios'
 
 export const useStorage = () => {
- const creaeteBucket = async () => {
-  await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/storage/bucket`)
- }
+  const creaeteBucket = async () => {
+    await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/storage/bucket`)
+  }
 
- const deleteBucket = async () => {
-  await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/storage/bucket`)
- }
+  const deleteBucket = async () => {
+    await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/storage/bucket`)
+  }
 
- const putObject = async (file: File | null) => {
-  if (!file) return
+  const putObject = async (file: File | null) => {
+    if (!file) return
 
-  const formData = new FormData();
-  formData.append('file', file);
+    const formData = new FormData()
+    formData.append('file', file)
 
-  await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/storage/object`, formData, {
-   headers: {
-    'Content-Type': 'multipart/form-data',
-   },
-  })
- }
+    await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/storage/object`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    )
+  }
 
- return {
-  creaeteBucket,
-  deleteBucket,
-  putObject
- }
+  return {
+    creaeteBucket,
+    deleteBucket,
+    putObject,
+  }
 }
